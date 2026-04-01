fa0902060505a9f36064f9ff2a948dc5cb392b28
twinbytestudios-lang <twinbytestudios@gmail.com>
2026-03-31 19:19:12 -0500

    Delete addons/dungeon_plugin.gd

--- addons/dungeon_plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("DungeonGen2D", "Node2D", preload("generator_node.gd"), null)
-
-func _exit_tree():
-	remove_custom_type("DungeonGen2D")
